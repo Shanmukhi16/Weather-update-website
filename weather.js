2f892c0e1310a830d210ddb4f1dfa9455c8a5435
karri shanmukhi sai sridevi <141435446+Shanmukhi16@users.noreply.github.com>
2024-09-22 16:01:19 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,3 +1,11 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080);
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.search-form');
     form.addEventListener('submit', async (event) => {
